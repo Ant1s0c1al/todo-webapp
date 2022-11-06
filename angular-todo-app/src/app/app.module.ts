11297a35ffe4c6a009194639c504938a07d1073a
Ant1s0c1al <ant1s0c1al.opensource@gmail.com>
2022-11-06 22:34:03 +0000

    api: (get) rewuest, pulling data from database

--- angular-todo-app/src/app/app.module.ts
+++ angular-todo-app/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
@@ -8,6 +10,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { BtnComponent } from './components/btn/btn.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
+// [Free] Icons
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
@@ -22,7 +25,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-todo-app/src/app/services/todo.service.ts
+++ angular-todo-app/src/app/services/todo.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+// [After] >>> Must Manually Add to app.module.ts HttpClientModule
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+// [Before]: db.json
 import { Observable, of } from 'rxjs';
 import { Todo } from '../Todo';
 import { TODOS } from '../mock-todos';
@@ -7,8 +11,11 @@ import { TODOS } from '../mock-todos';
   providedIn: 'root'
 })
 export class TodoService {
+  // (private) Property for API URL
+  private apiUrl = 'http://localhost:5000/todos'
 
-  constructor() { }
+  // NOTE: this.http >>> (Method) GET, POST, DELETE etc
+  constructor(private http: HttpClient) { }
 
   /* (Method)
   getTodos(): Todo[] {
@@ -17,7 +24,12 @@ export class TodoService {
   // >>> Turned into Observable <<<
   getTodos(): Observable<Todo[]> {
     // return Observable Directly
+    /* [Before]: private apiUrl + constructor()
     const todos = of(TODOS);
-    return todos;
+    return todos; */
+
+    // NOTE: return (data) from database
+    // ### [GET] Request ###
+    return this.http.get<Todo[]>(this.apiUrl);
   }
 }
