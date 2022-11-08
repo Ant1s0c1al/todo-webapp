a6ff76f697c218dc18d2dd00d3b0d4f7a571e653
Ant1s0c1al <ant1s0c1al.opensource@gmail.com>
2022-11-08 21:23:59 +0000

    build: html forms input text, checkbox important, save button

--- angular-todo-app/src/app/components/new-todo/new-todo.component.ts
+++ angular-todo-app/src/app/components/new-todo/new-todo.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-todo.component.scss']
 })
 export class NewTodoComponent implements OnInit {
+  // Add: Input html fields as [Properties]
+  text: string;
+  important: boolean = false;
 
   constructor() { }
 
