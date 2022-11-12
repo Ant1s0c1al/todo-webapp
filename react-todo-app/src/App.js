e0372d2be3fc19182df4a2b82c76779584554c4a
Ant1s0c1al <ant1s0c1al.opensource@gmail.com>
2022-11-12 10:46:06 +0000

    build: react hook useState used to create hardcoded todos list

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import NavBar from './components/NavBar';
 //import logo from './logo.svg';
 import './App.scss';
 
 function App() {
+  // React(Hooks) useState | Returns a stateful value
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Lorem qui minim labore adipisicin',
+      important: false,
+    },
+    {
+      id: 2,
+      text: 'Pora qui minirwwm lskodre pisiiwf',
+      important: true,
+    },
+    {
+      id: 3,
+      text: 'Boram qumddi miniffm labore adipd',
+      important: true,
+    },
+  ])
+
+  // #Function • Delete
+  // #Function • Important
+  // #Function • Create
+
   return (
     <>
 
