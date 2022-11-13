0c94ea90ceb4455051b78a8db98e33fa2d09cfc5
Ant1s0c1al <ant1s0c1al.opensource@gmail.com>
2022-11-13 18:31:27 +0000

    build: removed h1, cleaner ui

--- react-todo-app/src/components/CreateTodoHeader.js
+++ react-todo-app/src/components/CreateTodoHeader.js
@@ -4,8 +4,6 @@ import CustomBtn from './CustomBtn';
 const CreateTodoHeader = ({ onCreate, showCreate }) => {
   return (
     <>
-      <h1 className='mt-4 text-center'>Create New Todo</h1>
-
       <CustomBtn
         color={showCreate ? 'danger' : 'primary'}
         text={showCreate ? 'Close' : 'Create A Todo'}
