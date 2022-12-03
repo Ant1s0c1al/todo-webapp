a2ed788d46987dfcbb47c66c480d8b148a52e8bb
Ant1s0c1al <ant1s0c1al.opensource@gmail.com>
2022-12-03 10:29:46 +0000

    [STYLE]: UI Update Clean Minimal Design

--- next-jsx-todo-app/app/todo-item.jsx
+++ next-jsx-todo-app/app/todo-item.jsx
@@ -1,12 +1,16 @@
 /* [CLIENT] COMPONENT */
 'use client';
+import styles from './page.module.scss';
 
 // Pass in 'todo' as a prop
 export default function TodoItem({ todo }) {
   return (
     <>
-      <div className='text-center'>
-        <p>{todo.text}</p>
+      <div className={styles.grid}>
+        <a className={`${styles.card}`} href='#'>
+          <h2>#{todo.id}</h2>
+          <p>{todo.text}</p>
+        </a>
       </div>
     </>
   );
