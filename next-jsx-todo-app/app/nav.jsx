3c1112a07625e0fc114fdf5fd64b78a7cf579250
Ant1s0c1al <ant1s0c1al.opensource@gmail.com>
2022-12-03 05:35:25 +0000

    [BUILD]: Initial Setup W/ SASS Support, Bootstrapv5, (experimental)App Directory, json-server

---
+++ next-jsx-todo-app/app/nav.jsx
@@ -0,0 +1,55 @@
+/* [CLIENT] COMPONENT | Navigation Bar */
+'use client';
+
+import Link from 'next/link';
+import React, { useEffect } from 'react';
+
+export default function Nav() {
+
+        useEffect(() => {
+                import('bootstrap/dist/js/bootstrap');
+        }, [])
+
+        return (
+                <>
+                        <nav className='navbar navbar-dark navbar-expand-md bg-black'>
+                                <div className='container-fluid'>
+
+                                        <Link
+                                                className='navbar-brand fw-bold d-block-inline py-2'
+                                                href='/'
+                                        > <span>&#x1F410;</span> NEXT | TODO
+                                        </Link>
+
+                                        <button
+                                                className='navbar-toggler'
+                                                type='button'
+                                                data-bs-toggle='collapse'
+                                                data-bs-target='#navbarNav'
+                                                aria-controls='navbarNav'
+                                                aria-expanded='false'
+                                                aria-label='Toggle navigation'
+                                        > <span className='navbar-toggler-icon'></span>
+                                        </button>
+
+                                        <div className='collapse navbar-collapse' id='navbarNav'>
+                                                <ul className='navbar-nav'>
+
+                                                        <li className='nav-item'>
+                                                                <Link
+                                                                        className='nav-link active'
+                                                                        aria-current='page'
+                                                                        href='/'>
+                                                                        HOME
+                                                                </Link>
+                                                        </li>
+
+                                                </ul>
+
+                                        </div>
+
+                                </div>
+                        </nav>
+                </>
+        );
+}
---
+++ next-jsx-todo-app/app/page.jsx
@@ -0,0 +1,12 @@
+/* [Home] page.jsx | COMPONENT */
+export default function Home() {
+  return (
+    <>
+      <main>
+        <section className='text-center'>
+          HOMEPAGE app/page.jsx
+        </section>
+      </main>
+    </>
+  );
+}
