c9dba9156a9137f04632dbccb201c1209be2194d
Ant1s0c1al <ant1s0c1al.opensource@gmail.com>
2022-12-03 05:38:44 +0000

    fix(temp): BUG - fixed indentation

--- next-jsx-todo-app/app/nav.jsx
+++ next-jsx-todo-app/app/nav.jsx
@@ -6,50 +6,50 @@ import React, { useEffect } from 'react';
 
 export default function Nav() {
 
-        useEffect(() => {
-                import('bootstrap/dist/js/bootstrap');
-        }, [])
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap');
+  }, [])
 
-        return (
-                <>
-                        <nav className='navbar navbar-dark navbar-expand-md bg-black'>
-                                <div className='container-fluid'>
+  return (
+    <>
+      <nav className='navbar navbar-dark navbar-expand-md bg-black'>
+        <div className='container-fluid'>
 
-                                        <Link
-                                                className='navbar-brand fw-bold d-block-inline py-2'
-                                                href='/'
-                                        > <span>&#x1F410;</span> NEXT | TODO
-                                        </Link>
+          <Link
+            className='navbar-brand fw-bold d-block-inline py-2'
+            href='/'
+          > <span>&#x1F410;</span> NEXT | TODO
+          </Link>
 
-                                        <button
-                                                className='navbar-toggler'
-                                                type='button'
-                                                data-bs-toggle='collapse'
-                                                data-bs-target='#navbarNav'
-                                                aria-controls='navbarNav'
-                                                aria-expanded='false'
-                                                aria-label='Toggle navigation'
-                                        > <span className='navbar-toggler-icon'></span>
-                                        </button>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarNav'
+            aria-controls='navbarNav'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          > <span className='navbar-toggler-icon'></span>
+          </button>
 
-                                        <div className='collapse navbar-collapse' id='navbarNav'>
-                                                <ul className='navbar-nav'>
+          <div className='collapse navbar-collapse' id='navbarNav'>
+            <ul className='navbar-nav'>
 
-                                                        <li className='nav-item'>
-                                                                <Link
-                                                                        className='nav-link active'
-                                                                        aria-current='page'
-                                                                        href='/'>
-                                                                        HOME
-                                                                </Link>
-                                                        </li>
+              <li className='nav-item'>
+                <Link
+                  className='nav-link active'
+                  aria-current='page'
+                  href='/'>
+                  HOME
+                </Link>
+              </li>
 
-                                                </ul>
+            </ul>
 
-                                        </div>
+          </div>
 
-                                </div>
-                        </nav>
-                </>
-        );
+        </div>
+      </nav>
+    </>
+  );
 }
