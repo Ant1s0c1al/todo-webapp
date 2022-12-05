6c6923eb01e3b75479b1e145a8d9240c1bfeac98
Ant1s0c1al <ant1s0c1al.opensource@gmail.com>
2022-12-05 04:08:34 +0000

    [Backup]: convert to typescript

--- next-jsx-todo-app/app/todo-item.jsx
+++ next-jsx-todo-app/app/todo-item.jsx
@@ -1,9 +1,38 @@
 /* [CLIENT] COMPONENT */
 'use client';
+import { useRouter } from 'next/navigation';
 import styles from './page.module.scss';
+
+// (TEMPRARY) Method Till New Method Implemented!
+async function getData(id) {
+  const data = await fetch(`http://localhost:5000/todos/${id}`);
+
+  // Recommendation: handle errors
+  if (!data.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return data.json();
+}
+
+// [PUT] Request | Toggle Boolean Value >>> completed
+async function updateData(id, refresh) {
+  const toggleTodoId = await getData(id);
+  const updateTodo = { ...toggleTodoId, completed: !toggleTodoId.completed }
+
+  await fetch(`http://localhost:5000/todos/${id}`, {
+    method: 'POST',
+    body: JSON.stringify({ updateTodo }),
+  });
+
+  refresh();
+}
+
 
 // Pass in 'todo' as a prop
 export default function TodoItem({ todo }) {
+  const router = useRouter();
+
   return (
     <>
       <div className={styles.grid}>
@@ -13,10 +42,14 @@ export default function TodoItem({ todo }) {
             <input
               type='checkbox'
               className='form-check-input'
-              id='exampleCheck1'
+              checked={todo.completed}
+              onChange={(e) => updateData(todo.id, e.target.checked, router.refresh)}
             />
           </div>
-          <span className='badge rounded-pill text-bg-primary text-white'>+XP 100</span>
+          <span
+            className='badge rounded-pill text-bg-primary text-white'
+          > +XP 100
+          </span>
           <p className='pt-3'>{todo.text}</p>
         </div>
       </div>
