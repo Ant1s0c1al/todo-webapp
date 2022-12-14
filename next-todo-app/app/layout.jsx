966071684994ab623fead185562d941447895b31
Ant1s0c1al <ant1s0c1al.opensource@gmail.com>
2022-12-14 00:51:46 +0000

    [Build]: Added <main> Semantic HTML Tag | SEO

--- next-todo-app/app/layout.jsx
+++ next-todo-app/app/layout.jsx
@@ -4,19 +4,19 @@ import NavBar from './navbar';
 import Footer from './footer';
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang='en'>
-      {/*
+    return (
+        <html lang='en'>
+            {/*
         <head /> will contain the components 
         returned by the nearest parent head.jsx. 
         https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>
-        <NavBar />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  );
+            <head />
+            <body>
+                <NavBar />
+                <main>{children}</main>
+                <Footer />
+            </body>
+        </html>
+    );
 }
