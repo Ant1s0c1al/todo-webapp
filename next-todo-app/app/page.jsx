7ff67bc26d0a7d4561c7d14aecca5e1aab5266b1
Ant1s0c1al <ant1s0c1al.opensource@gmail.com>
2022-12-14 01:08:29 +0000

    [Build]: Added React Suspense & Call Custom Loading Component In fallback

--- next-todo-app/app/page.jsx
+++ next-todo-app/app/page.jsx
@@ -1,12 +1,16 @@
 /* [Home] */
-import styles from './page.module.scss';
+import { Suspense } from 'react';
+import Loading from './loading';
+//import styles from './page.module.scss';
 import Todos from './todos';
 
 export default function Home() {
   return (
     <>
       <section className='container mt-4'>
-        <Todos />
+        <Suspense fallback={<Loading />}>
+          <Todos />
+        </Suspense>
       </section>
     </>
   );
