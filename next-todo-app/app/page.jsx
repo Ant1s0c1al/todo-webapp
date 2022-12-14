19af29f2e4a348049effdb384ba21d89bf02faa8
Ant1s0c1al <ant1s0c1al.opensource@gmail.com>
2022-12-14 00:53:54 +0000

    [Build]: Imported Todos List To HomePage

--- next-todo-app/app/page.jsx
+++ next-todo-app/app/page.jsx
@@ -1,9 +1,13 @@
 /* [Home] */
 import styles from './page.module.scss';
+import Todos from './todos';
 
 export default function Home() {
   return (
     <>
+      <section className='container mt-4'>
+        <Todos />
+      </section>
     </>
   );
 }
